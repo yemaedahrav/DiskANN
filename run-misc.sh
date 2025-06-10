34cb6345d24883b7ae7c65ac93e8a61020ea0f8c
Amey <t-avarhade@microsoft.com>
2025-06-10 09:32:15 +0000

    Res: Add more results

--- run-misc.sh
+++ run-misc.sh
@@ -6,5 +6,8 @@ cmake -DCMAKE_BUILD_TYPE=Release .. && make -Bj
 # ./apps/utils/compute_groundtruth --dist_fn l2 --data_type float --base_file /nvmessd1/fbv4/avarhade/datasets/prec1M_normalized.bin --query_file /nvmessd1/fbv4/avarhade/datasets/prec1M_queries_normalized.bin --gt_file /nvmessd1/fbv4/avarhade/datasets/prec1M_normalized_gt200 --K 200
 # ./apps/utils/compute_groundtruth --dist_fn l2 --data_type float --base_file /nvmessd1/fbv4/avarhade/datasets/wiki1M_normalized.bin --query_file /nvmessd1/fbv4/avarhade/datasets/wiki1M_queries_normalized.bin --gt_file /nvmessd1/fbv4/avarhade/datasets/wiki1M_normalized_gt200 --K 200
 
-./apps/utils/compute_groundtruth --dist_fn l2 --data_type float --base_file /home/t-avarhade/prec1M/prec1M_normalized.bin --query_file /home/t-avarhade/prec1M/prec1M_queries_normalized.bin --gt_file /home/t-avarhade/prec1M/prec1M_normalized_gt100 --K 100
-./apps/utils/compute_groundtruth --dist_fn l2 --data_type float --base_file /home/t-avarhade/prec1M/prec1M_normalized.bin --query_file /home/t-avarhade/prec1M/prec1M_queries_normalized.bin --gt_file /home/t-avarhade/prec1M/prec1M_normalized_gt200 --K 200
+# ./apps/utils/compute_groundtruth --dist_fn l2 --data_type float --base_file /home/t-avarhade/prec1M/prec1M_normalized.bin --query_file /home/t-avarhade/prec1M/prec1M_queries_normalized.bin --gt_file /home/t-avarhade/prec1M/prec1M_normalized_gt100 --K 100
+# ./apps/utils/compute_groundtruth --dist_fn l2 --data_type float --base_file /home/t-avarhade/prec1M/prec1M_normalized.bin --query_file /home/t-avarhade/prec1M/prec1M_queries_normalized.bin --gt_file /home/t-avarhade/prec1M/prec1M_normalized_gt200 --K 200
+
+
+./apps/utils/compute_groundtruth --dist_fn l2 --data_type float --base_file /home/t-avarhade/prec1M/prec1M_normalized_hybrid_r128_l200_m5_s100000_h0.75_f0.3 --query_file /home/t-avarhade/prec1M/prec1M_queries_normalized.bin --gt_file /home/t-avarhade/prec1M/prec1M_normalized_hybrid_r128_l200_m5_s100000_h0.75_f0.3_gt200 --K 200
--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ data_path="${file_base_path}/prec1M_normalized_hybrid_r128_l200_m5_s100000_h0.75
 gt_file="${file_base_path}/prec1M_normalized_hybrid_r128_l200_m5_s100000_h0.75_f0.3_gt200"
 query_file="${file_base_path}/prec1M_queries_normalized.bin"
 
-./apps/build_memory_index --data_path $data_path --data_type float --dist_fn l2 --index_path_prefix "${file_base_path}/index/test" -R 64 -L 100 -T 96 >> /home/t-avarhade/Amey/DiskANN/prec1M-baseline/prec1M_normalized_r64_l00.txt
-./apps/search_memory_index --data_type float --dist_fn l2 --index_path_prefix "${file_base_path}/index/test" --gt_file $gt_file --query_file $query_file --result_path /home/t-avarhade/Dump/tmp -K 200 -L 200 250 300 400 500 -T 96 >> /home/t-avarhade/Amey/DiskANN/prec1M-baseline/prec1M_normalized_r64_l00.txt
+./apps/build_memory_index --data_path $data_path --data_type float --dist_fn l2 --index_path_prefix "${file_base_path}/index/test" -R 128 -L 200 -T 1 >> /home/t-avarhade/Amey/DiskANN/prec1M-baseline/prec1M_normalized_r128_l200.txt
+./apps/search_memory_index --data_type float --dist_fn l2 --index_path_prefix "${file_base_path}/index/test" --gt_file $gt_file --query_file $query_file --result_path /home/t-avarhade/Dump/tmp -K 200 -L 200 250 300 400 500 -T 1 >> /home/t-avarhade/Amey/DiskANN/prec1M-baseline/prec1M_normalized_r128_l200.txt
