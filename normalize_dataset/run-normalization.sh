3a5900a0489bf0d18621e3e41693ae20c1786525
Amey <t-avarhade@microsoft.com>
2025-06-19 04:44:52 +0000

    Res: Add code to normalize a dataset

---
+++ normalize_dataset/run-normalization.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+input_base_path="/home/t-avarhade/big-ann-benchmarks/data/wikipedia_cohere"
+output_base_path="/home/t-avarhade/wiki_normalized"
+
+data_file="${input_base_path}/wikipedia_base.bin"
+normalized_data_file="${output_base_path}/wiki35M_normalized.bin"
+query_file="${input_base_path}/wikipedia_query.bin"
+normalized_query_file="${output_base_path}/wiki_normalized_query.bin"
+
+gt_file="${output_base_path}/wiki35M_gt200.bin"
+
+# g++ -o normalize_vectors normalize_vectors.cpp
+# ./normalize_vectors $data_file $normalized_data_file $query_file $normalized_query_file > normalize_logs.txt
+g++ -o normalize_verify normalize_verify.cpp
+
+./normalize_verify $normalized_data_file >> normalize_logs.txt
+./normalize_verify $normalized_query_file >> normalize_logs.txt
+
+rm normalize_vectors normalize_verify
+
+# cd ../build
+# cmake -DCMAKE_BUILD_TYPE=Release .. && make -Bj
+
+# ./apps/utils/compute_groundtruth --data_type float --dist_fn l2 --base_file $normalized_data_file --query_file $normalized_query_file --gt_file $gt_file --K 200
+
--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 cd build
 # cmake -DCMAKE_BUILD_TYPE=Release .. && make -Bj
 
-file_base_path="/home/t-avarhade/prec1M"
-data_path="${file_base_path}/index/prec1M_normalized_hybrid_r128_l200_m5_s100000_h1.5_f0.3_dataset"
-gt_file="${file_base_path}/index/prec1M_normalized_hybrid_r128_l200_m5_s100000_h1.5_f0.3_gt200"
-query_file="${file_base_path}/prec1M_queries_normalized.bin"
+r=128
+l=200
+file_base_path="/home/t-avarhade"
+data_file="${file_base_path}/wiki_normalized/wiki35M_normalized.bin"
+index_file="${file_base_path}/wiki_normalized/index/wiki35M_normalized_r${r}_l${l}"
+gt_file="${file_base_path}/wiki_normalized/wiki35M_gt200.bin"
+query_file="${file_base_path}/wiki_normalized/wiki_normalized_query.bin"
+log_file="${file_base_path}/Amey/DiskANN/wiki35Mnormalized/wiki35M_r${r}_l${l}.txt"
 
-./apps/build_memory_index --data_path $data_path --data_type float --dist_fn l2 --index_path_prefix "${file_base_path}/index/test" -R 128 -L 200 -T 96 >> /home/t-avarhade/Amey/DiskANN/prec1M-baseline/prec1M_normalized_r128_l200.txt
-./apps/search_memory_index --data_type float --dist_fn l2 --index_path_prefix "${file_base_path}/index/test" --gt_file $gt_file --query_file $query_file --result_path /home/t-avarhade/Dump/tmp -K 200 -L 200 250 300 400 500 -T 96 >> /home/t-avarhade/Amey/DiskANN/prec1M-baseline/prec1M_normalized_r128_l200.txt
+./apps/build_memory_index --data_path $data_file --data_type float --dist_fn l2 --index_path_prefix $index_file -R $r -L $l -T 96 > $log_file
+./apps/search_memory_index --data_type float --dist_fn l2 --index_path_prefix $index_file --gt_file $gt_file --query_file $query_file --result_path /home/t-avarhade/Dump/tmp -K 200 -L 200 250 300 400 500 -T 96 >> $log_file
