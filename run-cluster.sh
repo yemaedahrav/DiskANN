18c4444c03c8a020fe3f3504cbd88fca0a010ccd
Amey <t-avarhade@microsoft.com>
2025-08-06 05:27:33 +0000

    Misc: Refactor repo structure

--- run-cluster.sh
+++ run-cluster.sh
@@ -28,3 +28,15 @@ log_path="${home}/Amey/DiskANN/openai/2pass_cluster_r${R}_l${L}.txt"
 ./apps/search_memory_index --data_type float --dist_fn l2 --index_path_prefix $index_path --cluster_path $cluster_path --gt_file $gt_file --query_file $query_file --result_path ${home}/Dump/tmp -K 25 -L 25 50 100 -T ${T} >> $log_path
 
 rm $cluster_path $index_path
+
+
+
+# PREC1M (FBV4) NORMALIZED
+# file_base_path="/nvmessd1/fbv4/avarhade/datasets"
+# data_path="${file_base_path}/prec1M_normalized.bin"
+# gt_file="${file_base_path}/prec1M_normalized_gt100"
+# query_file="${file_base_path}/prec1M_queries_normalized.bin"
+# result_path="/home/rakri/avarhade/Dump/tmp"
+
+# ./apps/build_memory_index --data_path $data_path --data_type float --dist_fn l2 --index_path_prefix /nvmessd1/fbv4/avarhade/clustering/prec1M_memory_index_clustering_r64_l100_mcs1_pm100_t0 --cluster_path /nvmessd1/fbv4/avarhade/clustering/prec1M_cluster_mapping_r64_l100_mcs1_pm100_t0.bin -R 64 -L 100 -T 48 >> /home/rakri/avarhade/DiskANN/prec1M/prec1M_clustering_r64_l100.txt
+# ./apps/search_memory_index --data_type float --dist_fn l2 --index_path_prefix /nvmessd1/fbv4/avarhade/clustering/prec1M_memory_index_clustering_r64_l100_mcs1_pm100_t0 --cluster_path /nvmessd1/fbv4/avarhade/clustering/prec1M_cluster_mapping_r64_l100_mcs1_pm100_t0.bin --gt_file $gt_file --query_file $query_file --result_path $result_path -K 100 -L 100 150 200 250 300 500 -T 48 >> /home/rakri/avarhade/DiskANN/prec1M/prec1M_clustering_r64_l100.txt
